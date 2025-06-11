f58e1eec8afd629759365eee1a42ba192a534529
Nguyễn Văn Tín <bguyt46@gmail.com>
2025-06-12 00:31:55 +0700

    build: init serve

---
+++ server/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // đọc .env tự động
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'), // lấy từ .env
+      }),
+      inject: [ConfigService],
+    }),
+    PostModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
---
+++ server/src/post/post.resolver.ts
@@ -0,0 +1,34 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { PostService } from './post.service';
+import { Post } from './entities/post.entity';
+import { CreatePostInput } from './dto/create-post.input';
+import { UpdatePostInput } from './dto/update-post.input';
+
+@Resolver(() => Post)
+export class PostResolver {
+  constructor(private readonly postService: PostService) { }
+
+  @Query(() => [Post])
+  async posts() {
+    return this.postService.findAll();
+  }
+
+  @Query(() => Post, { name: 'post' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.postService.findOne(id);
+  }
+  @Mutation(() => Post)
+  createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
+    return this.postService.create(createPostInput);
+  }
+
+  @Mutation(() => Post)
+  updatePost(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
+    return this.postService.update(updatePostInput.id, updatePostInput);
+  }
+
+  @Mutation(() => Post)
+  removePost(@Args('id', { type: () => Int }) id: number) {
+    return this.postService.remove(id);
+  }
+}
