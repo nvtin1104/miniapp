a698fee8d7b4502ec37312eb8c6be9579c1bfbd7
Nguyễn Văn Tín <bguyt46@gmail.com>
2025-06-19 19:54:21 +0700

    feat: chuyển token vào env

--- server/src/ngrok.config.ts
+++ server/src/ngrok.config.ts
@@ -1,11 +1,13 @@
 import { connect } from '@ngrok/ngrok';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export const startNgrok = async (port: number) => {
   try {
     const tunnel = await connect({
       addr: port,
-      authtoken: '2iHiLgBTZuQ6d53jzKpTJxU5inL_6Fh6rYAFGkHU4xzRMvoCG', // hoặc bỏ nếu không cần
-      domain: 'raccoon-destined-bee.ngrok-free.app',
+      authtoken: process.env.NGROK_AUTH_TOKEN, // hoặc bỏ nếu không cần
+      domain: process.env.SERVER_DOMAIN,
     });
 
     return tunnel.url();
